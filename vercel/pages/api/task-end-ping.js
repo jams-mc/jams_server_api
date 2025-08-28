60ccc3d12404724316d5de91724e21f9dcaae46c
jams-mc <jams_mineword@int.pl>
2025-08-28 15:54:24 +0200

    Update task-end-ping.js

--- vercel/pages/api/task-end-ping.js
+++ vercel/pages/api/task-end-ping.js
@@ -5,6 +5,7 @@ export default async function handler(req, res) {
     console.log("Method not allowed:", req.method);
     return res.status(405).json({ error: "Method not allowed" });
   }
+console.log("hello");
 
   const { data_array, result_url } = req.body;
 
